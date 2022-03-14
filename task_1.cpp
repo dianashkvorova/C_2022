843e62facb31dbe6aedafb974729d94844e3bc94
dianashkvorova <101040679+dianashkvorova@users.noreply.github.com>
2022-03-14 15:39:32 +0800

    Create task_1.cpp
    Задание 1. (типы данных, арифметические операторы)

---
+++ task_1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a=555;
+    float b=10.5;
+    char c='5';
+    double d=555.5;
+   printf("%d; ", a);
+   printf("%f; ", b);
+   printf("%d; ", c);
+   printf("%f\n ", d);
+   int s=a/10; printf("%d; ", s);
+   double p=d*3; printf("%f ", p);
+   float f=b+4; printf("%f; ", f);
+   int k=a%10; printf("%d; ", k);
+    return 0;
+}
