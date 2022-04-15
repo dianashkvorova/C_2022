6f1209ecc8fa990d124dc4939979e80db416fc14
dianashkvorova <101040679+dianashkvorova@users.noreply.github.com>
2022-04-15 23:07:10 +0800

    Update task_8.cpp

--- task_8.cpp
+++ task_8.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int Sort(int a){
-    int *p=&a;
-    return *p;
-}
-int main()
-{   int a[10] = {1, 8, 3, 6, 5, 4, 9, 2, 8, 12};
+void Sort(int a[10])
+{
     for (int i=0;i<10;i++){
         for (int j = i + 1; j < 10; j++)
         if (a[j] > a[i]) swap (a[i],a[j]);
-    cout << Sort(a[i])<< ' ';}
+        cout << a[i]<< ' ';}
+}
+int main()
+{
+    int a[10]={1, 8, 3, 6, 5, 4, 9, 2, 8, 12};
+    Sort(a); 
     return 0;
 }
